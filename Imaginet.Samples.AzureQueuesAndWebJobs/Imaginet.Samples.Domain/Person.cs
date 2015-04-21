c51f7889c8a7565e0e6bc3fe360a63611cf4f1f9
James Chambers <james@jameschambers.com>
2015-04-21 08:35:33 -0500

    submitting to queues - WIP
    just the first part of this is online, submitting data to the queue

---
+++ Imaginet.Samples.AzureQueuesAndWebJobs/Imaginet.Samples.Domain/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imaginet.Samples.Domain
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ Imaginet.Samples.AzureQueuesAndWebJobs/Imaginet.Samples.Queues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Imaginet.Samples.Domain;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Imaginet.Samples.Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["AzureWebJobsServiceBus"].ConnectionString;
+            var client = QueueClient.CreateFromConnectionString(connectionString, "signups");
+            var person = new Person { Name = "James Chambers", EmailAddress = "jamesc@imaginet.com" };
+            client.Send(new BrokeredMessage(person));
+        }
+    }
+}
